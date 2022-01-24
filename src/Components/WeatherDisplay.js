c362977b4ac8c1e4ebe505f289d0ccea92cbba0c
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-01-24 22:28:57 +0200

    implemented axios to fetch the weather data

--- src/Components/WeatherDisplay.js
+++ src/Components/WeatherDisplay.js
@@ -1,6 +1,29 @@
 import React from "react";
+import axios from "axios";
 
-const WeatherDisplay = ({lat}) => {
-  return <div>{lat}</div>;
-};
+class WeatherDisplay extends React.Component {
+  state = { place: "", temp: null };
+
+  async getWeather() {
+    const key = "5011a2f8dc2263d89aa88739a65bd335";
+    const result = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&appid=${key}`
+    );
+    const data = await result.data;
+    this.setState({
+      place: data.name,
+      tempF: data.main.temp,
+      tempC: (data.main.temp - 32) * 5/9,
+      icon: data.weather[0].icon,
+    });
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  render() {
+    return <div>{this.state.temp}</div>;
+  }
+}
 export default WeatherDisplay;
--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import Spinner from "./Components/Spinner";
 import WeatherDisplay from "./Components/WeatherDisplay";
 
 class App extends React.Component {
-  state = { lat: null, errorMessage: "" };
+  state = { lat: null, lon: null, errorMessage: "" };
 
   componentDidMount() {
     //get the user location
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
   renderContent() {
     if (this.state.lat) {
-      return <WeatherDisplay lat = {this.state.lat}/>;
+      return <WeatherDisplay lat={this.state.lat} lon={this.state.lon} />;
     } else if (this.state.errorMessage) {
       return <div>{this.state.errorMessage}</div>;
     } else {
